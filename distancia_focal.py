0adf8e5e9940ccb11496dfa00c0b723979d1afb5
alandri <alandri33@gmail.com>
2023-09-22 23:37:22 +0100

    cambios

--- distancia_focal.py
+++ distancia_focal.py
@@ -3,60 +3,78 @@ import numpy as np
 import cv2
 import math
 
-# Leer imagen
-img = cv2.imread("botella.jpg")
+# Inicializar la cámara
+camara = cv2.VideoCapture(0)  # 0 representa la cámara web predeterminada (puedes cambiarlo si tienes múltiples cámaras)
 
-# Convertir a escala de grises
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if not camara.isOpened():
+    print("Error: No se pudo abrir la cámara.")
+else:
+    # Capturar un fotograma de la cámara
+    ret, frame = camara.read()
 
-# Aplicar filtro de Canny
-edges = cv2.Canny(gray, 50, 150)
+    if ret:
+       
+        cv2.imwrite("captura.jpg", frame)
 
-# Encontrar contornos
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Leer imagen
+        img = cv2.imread("captura.jpg")
 
-# Dibujar contornos sobre la imagen original
-cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+        # Convertir a escala de grises
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Calcular el área de cada contorno
-areas = [cv2.contourArea(c) for c in contours]
+        # Aplicar filtro de Canny
+        edges = cv2.Canny(gray, 50, 150)
 
-# Seleccionar los dos contornos más grandes
-sorted_areas = np.argsort(areas)
-cnt1 = contours[sorted_areas[-1]] # Objeto a medir
-cnt2 = contours[sorted_areas[-2]] # Objeto de referencia
+        # Encontrar contornos
+        contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Calcular el ancho en píxeles de cada contorno
-rect1 = cv2.minAreaRect(cnt1)
-box1 = cv2.boxPoints(rect1)
-box1 = np.int0(box1)
-w1 = max(rect1[1]) # Ancho del objeto a medir
+        # Dibujar contornos sobre la imagen original
+        cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
 
-rect2 = cv2.minAreaRect(cnt2)
-box2 = cv2.boxPoints(rect2)
-box2 = np.int0(box2)
-w2 = max(rect2[1]) # Ancho del objeto de referencia
+        # Calcular el área de cada contorno
+        areas = [cv2.contourArea(c) for c in contours]
 
-# Dibujar los rectángulos mínimos sobre la imagen original
-cv2.drawContours(img,[box1],0,(0,0,255),2)
-cv2.drawContours(img,[box2],0,(255,0,0),2)
+        # Seleccionar los dos contornos más grandes
+        sorted_areas = np.argsort(areas)
+        cnt1 = contours[sorted_areas[-1]] # Objeto a medir
+        cnt2 = contours[sorted_areas[-2]] # Objeto de referencia
 
-# Definir el tamaño del objeto de referencia (hoja A4) en centímetros
-W = 21
+        # Calcular el ancho en píxeles de cada contorno
+        rect1 = cv2.minAreaRect(cnt1)
+        box1 = cv2.boxPoints(rect1)
+        box1 = np.int0(box1)
+        w1 = max(rect1[1]) # Ancho del objeto a medir
 
-# Definir la distancia del objeto de referencia a la cámara en centímetros
-D = 30
+        rect2 = cv2.minAreaRect(cnt2)
+        box2 = cv2.boxPoints(rect2)
+        box2 = np.int0(box2)
+        w2 = max(rect2[1]) # Ancho del objeto de referencia
 
-# Calcular la distancia focal de la cámara
-f = (w2 * D) / W
+        # Dibujar los rectángulos mínimos sobre la imagen original
+        cv2.drawContours(img,[box1],0,(0,0,255),2)
+        cv2.drawContours(img,[box2],0,(255,0,0),2)
 
-# Calcular la distancia del objeto a medir a la cámara
-d = (W * f) / w1
+        # Definir el tamaño del objeto de referencia (hoja A4) en centímetros
+        W = 21
 
-# Mostrar el resultado en la pantalla
-print("La distancia del objeto a medir a la cámara es: {:.2f} cm".format(d))
+        # Definir la distancia del objeto de referencia a la cámara en centímetros
+        D = 30
 
-# Mostrar la imagen con los contornos y los rectángulos
-cv2.imshow("Imagen", img)
-cv2.waitKey(0)
+        # Calcular la distancia focal de la cámara
+        f = (w2 * D) / W
+
+        # Calcular la distancia del objeto a medir a la cámara
+        d = (W * f) / w1
+
+        # Mostrar el resultado en la pantalla
+        print("La distancia del objeto a medir a la cámara es: {:.2f} cm".format(d))
+
+        # Mostrar la imagen con los contornos y los rectángulos
+        cv2.imshow("Imagen", img)
+        cv2.waitKey(0)
+
+    # Liberar la cámara
+    camara.release()
+
+# Cerrar las ventanas de OpenCV
 cv2.destroyAllWindows()
