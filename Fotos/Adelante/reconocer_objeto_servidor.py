c09eb23d7511ac34b5fa743794c3b5258e4e6856
RamiroGarciaGelsi <rggelsi@gmail.com>
2023-10-08 00:19:30 -0300

    reconocer_objeto_servidor_working_any_signal
    Fue testeado con imagenes de adelante y U, queda guardado con la imagen de adelante

--- Fotos/Adelante/reconocer_objeto_servidor.py
+++ Fotos/Adelante/reconocer_objeto_servidor.py
@@ -3,16 +3,19 @@ import cv2
 import numpy as np
 
 print(torch.__version__)
-image = cv2.imread('p/objeto_1.jpg')
+image = cv2.imread('p/objeto_0.jpg')
 
 #lectura modelo
 model=torch.hub.load('ultralytics/yolov5', 'custom',
-                      path='/model/carteles.pt')
+                      path='model\carteles.pt')
 
 
 detect=model(image)
 cv2.imshow('Detector de señales de transito', np.squeeze(detect.render()))
 
-t = cv2.watKey(5)
+while True:
+    key = cv2.waitKey(5)
+    if key == 27:  # Check if the pressed key is 'Esc' (ASCII code 27)
+        break
 
 cv2.destroyAllWindows()
